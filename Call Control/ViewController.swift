a68e8a07439f2397adf315fdb7d76ee7c6f5341a
Kurtis Hill <kurtis.hill212@gmail.com>
2019-01-30 16:27:25 -0700

    Initial Commit

---
+++ Call Control/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Call Control
+//
+//  Created by Kurtis Hill on 1/30/19.
+//  Copyright © 2019 Kurtis Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
